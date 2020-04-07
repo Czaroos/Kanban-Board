e1713ac82658797a6a868e274655e5f0e85b1cea
MichalzDev <51170703+MichalzDev@users.noreply.github.com>
2020-04-07 14:09:08 +0200

    Deployment ready fixed

---
+++ frontend/build/precache-manifest.563ba942de445c47c1e77c5286f25b0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d51c1eefc3b41929944fab8f9f63f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84052a555c85b4a4f0f",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "c1a31a3a868fed152821",
+    "url": "/static/css/main.5e7ae0b1.chunk.css"
+  },
+  {
+    "revision": "b84052a555c85b4a4f0f",
+    "url": "/static/js/2.14bfcb82.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.14bfcb82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a31a3a868fed152821",
+    "url": "/static/js/main.d580704d.chunk.js"
+  },
+  {
+    "revision": "3e4b0100d7a22b1cc792",
+    "url": "/static/js/runtime-main.3397a84a.js"
+  }
+]);
